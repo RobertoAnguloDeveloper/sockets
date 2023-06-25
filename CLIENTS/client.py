acf5c2278c46ff6c2ff6f234632e1d67853e9a37
RobertoAnguloDeveloper <rangulot@outlook.com>
2023-06-25 13:46:54 -0300

    First commit

--- CLIENTS/client.py
+++ CLIENTS/client.py
@@ -1,7 +1,7 @@
 import socket
 
-HOST = "127.0.0.1" # IP Server Address
-PORT = 65123 # Port to send requests
+HOST = "144.22.204.157" # IP Server Address
+PORT = 8080 # Port to send requests
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
